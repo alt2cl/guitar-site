0e3065b1088bf147beae5ccf398f0cc0e331d576
alt2cl <aldodiaz1983@gmail.com>
2023-02-28 23:38:04 -0300

    proyecto terminado sin css aun

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,11 +1,79 @@
+import {useEffect, useState} from 'react'
 import Layout from "@/components/layout";
-function Carrito(props) {
-    return (
-        <Layout>
-            <main className="container">
-                Carrito
-            </main>
+import Image from "next/image";
+function Carrito({carrito, actualizarCantidad, eliminarProducto}) {
+    const [total, setTotal] = useState(0)
 
+    //acumuador usamos reduce, como dependencia de carrito, reduce itera sobre cada "producto" y va sumando al total de "total"
+    useEffect(()=> {
+        const calculoTotal = carrito.reduce((total, producto) => total + (producto.cantidad* producto.precio), 0)
+        setTotal(calculoTotal)
+
+    },[carrito])
+    return (
+        <Layout title={"Carrito de compas"} >
+            <div>
+            <h1 className="heading">Carrito</h1>
+            <div>
+                <div className="grid grid-cols-12 gap-6">
+                    <main className="col-span-9">
+                        <div>
+                            <h2>Artículos</h2>
+                        </div>
+                        <div>
+                            {carrito.length === 0 ? 'Carrito vacio' : (
+                                carrito.map(producto=>(
+                                    <div key={producto.id} className='flex'>
+                                        <div>
+                                            <Image src={producto.imagen} width={120} height={200} alt={`Imagen de ${producto.nombre}`} />
+                                        </div>
+                                        <div className='flex-1'>
+                                            <p>{producto.nombre}</p>
+                                            <p>Precio: ${producto.precio}</p>
+                                            <p>Cantidad de productos: {producto.cantidad}</p>
+                                            <div className="cantidad-select">
+                                                
+                                                    <label htmlFor="">Cantidad</label>
+                                                    <select onChange={ e => actualizarCantidad({
+                                                        id:producto.id,
+                                                        cantidad: e.target.value,
+                                                    })} value={producto.cantidad}>
+                                                        <option value="1">1</option>
+                                                        <option value="2">2</option>
+                                                        <option value="3">3</option>
+                                                        <option value="4">4</option>
+                                                        <option value="5">5</option>
+                                                        <option value="6">6</option>
+                                                        <option value="7">7</option>
+                                                        <option value="8">8</option>
+                                                        <option value="9">9</option>
+                                                        <option value="10">10</option>
+                                                    </select>
+                                                
+
+                                            </div>
+                                            <p>Subtotal: $ {producto.cantidad * producto.precio}</p>
+                                        </div>
+                                        <button className='eliminar-btn col-span-1' type='button' onClick={()=>eliminarProducto(producto.id)}>
+                                            x
+                                        </button>
+                                        
+                                        
+                                    </div>
+
+                                ))
+                            )}
+                        </div>
+                    </main>
+                    <aside className="col-span-3">
+                        <h3>Resumend el pedido</h3>
+                        <p>Total a Pagar: ${total}</p>
+
+                    </aside>
+                </div>
+            </div>
+            </div>
+            
         </Layout>
         
     );
--- pages/guitarras/[id].js
+++ pages/guitarras/[id].js
@@ -1,20 +1,70 @@
+import { useState} from "react"
 import Layout from "@/components/layout";
 import Link from "next/link";
 import Image from "next/image";
 
-function Producto({guitarra}) {
+function Producto({guitarra, agregarCarrito}) {
+    const [cantidad, setCantidad] = useState(0)
     const {nombre, descripcion, imagen, precio} = guitarra[0]?.attributes
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if(cantidad < 1){
+
+            alert('Cantidad invalida')
+            return
+
+        }
+
+        //construir un objeto
+        const guitarraSeleccionada = {
+            id:guitarra[0].id,
+            imagen: imagen.data.attributes.formats.medium.url,
+            nombre,
+            precio,
+            cantidad
+        }
+
+        //pasando la informacion al carrito
+
+        agregarCarrito(guitarraSeleccionada)
+
+
+    }
+
     return (
         <Layout title={nombre} description={`Guitarra ${nombre}`}>
             <main>
-                <div className='grid grid-cols-2 gap-6'>
-                    <div>
-                    <Image src={imagen.data.attributes.url} alt={`Imagen ${nombre}`} width={600} height={400} />
+                <div className='grid grid-cols-7 gap-6'>
+                    <div className="flex justify-center col-span-3">
+                    <Image src={imagen.data.attributes.url} alt={`Imagen ${nombre}`} width={200} height={450} />
                     </div>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col col-span-4'>
                         <h3 className='mb-5 font-semibold'>{nombre}</h3>
                         <p className='mb-5'>{descripcion}</p>
                         <span>${precio}</span>
+                        <form onSubmit={handleSubmit} className="formulario border-gray-100 p-5 mt-10 bg-slate-300">
+                            <div className="grid grid-cols-2 gap-5 mb-5">
+                                <label htmlFor="cantidad">Cantidad</label>
+                                <select id="cantidad" onChange={e=> setCantidad(+e.target.value)}>
+                                    
+                                    <option value="0">--Seleccione--</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            <input type="submit" value="Agregar al carrito" className="block w-full p-3 text-center bg-black text-white" />
+                           
+                        </form>
                         
                     </div>
                 </div>
